dreamer-dead/MyLightpack

Make debug.h file common for all projects and move it to ../common. Extract printing utilities to common/PrintHelpers.hpp file. Sorted some includes.

Software/prismatic/SettingsWindow.cpp
@@ -24,27 +24,26 @@
  *
  */
 
+#include "SettingsWindow.hpp"
+
 #include <QtAlgorithms>
+#include <QScrollBar>
+#include <QStringBuilder>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QDesktopWidget>
-#include "LightpackApplication.hpp"
 
-#include "SettingsWindow.hpp"
-#include "ui_SettingsWindow.h"
-
-#include "Settings.hpp"
-#include "SpeedTest.hpp"
 #include "ColorButton.hpp"
 #include "LedDeviceManager.hpp"
-#include "enums.hpp"
-#include "debug.h"
+#include "LightpackApplication.hpp"
 #include "Plugin.hpp"
+#include "Settings.hpp"
 #include "SettingsDefaults.hpp"
+#include "SpeedTest.hpp"
+#include "common/DebugOut.hpp"
+#include "enums.hpp"
 #include "systrayicon/SysTrayIcon.hpp"
-#include <QStringBuilder>
-#include <QScrollBar>
-
+#include "ui_SettingsWindow.h"
 
 using namespace SettingsScope;
 

psieg/Lightpack

Software/src/SettingsWindow.cpp
@@ -24,26 +24,26 @@
  *
  */
 
+#include "SettingsWindow.hpp"
+
 #include <QtAlgorithms>
+#include <QScrollBar>
+#include <QMessageBox>
+#include <QStringBuilder>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QDesktopWidget>
-#include "LightpackApplication.hpp"
 
-#include "SettingsWindow.hpp"
-#include "ui_SettingsWindow.h"
-
-#include "Settings.hpp"
 #include "ColorButton.hpp"
 #include "LedDeviceManager.hpp"
-#include "enums.hpp"
-#include "debug.h"
+#include "LightpackApplication.hpp"
 #include "Plugin.hpp"
+#include "Settings.hpp"
+#include "SettingsDefaults.hpp"
+#include "common/DebugOut.hpp"
+#include "enums.hpp"
 #include "systrayicon/SysTrayIcon.hpp"
-#include <QStringBuilder>
-#include <QScrollBar>
-#include <QMessageBox>
-
+#include "ui_SettingsWindow.h"
 
 using namespace SettingsScope;
 